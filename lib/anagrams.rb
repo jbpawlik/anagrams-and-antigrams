7d59cc7b04504d4079fa5c3713ff78897367688a
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-16 12:46:48 -0700

    encapulate existing functions

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,49 +2,50 @@ require 'pry'
 
 module Text_Parsers
   def anagram_checker(str)
-    @text = str
-    uneven_input()
-      if uneven_input === true
+    # uneven_input()
+      if uneven_input(str) === true
         puts 'You need to enter the same number of characters if you want to know if something is an anagram!'
       else
-        vowel_checker()
-        if vowel_checker === false
-          puts "You need to input actual words!"
+        # vowel_checker()
+        if vowel_checker(str) === false
+          return "You need to input actual words!"
         else
-          is_anagram()
-          if is_anagram === false
+          # is_anagram()
+          if is_anagram(str) === false
             puts "This is not an anagram."
           else
             puts "Congratulations, these are anagrams of each other!"
+            return true
           end
         end
       end
     end
   end
 
-  def uneven_input()
+  def uneven_input(str)
     if @text.length != str.length
-    else
       return true
+    else
+      return false
     end
   end
 
-  def vowel_checker()
+  def vowel_checker(str)
     scan = str.scan(/[aeiouy]/)
     if scan.length > 0
       return true
     else
-      return
+      return false
     end
   end
 
-  def is_anagram()
+  def is_anagram(str)
     array1 = str.downcase.chars.sort
     array2 = @text.downcase.chars.sort
     if array1 === array2
       return true
     else
-      is_antigram()
+      is_antigram(str)
       if is_antigram === true
         return 'No letters match; this is an antigram!'
       else
