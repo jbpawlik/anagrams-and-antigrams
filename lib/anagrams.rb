a511ad3dfe79d307ef5e631c2efcf7561c8c5f3f
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-07-16 16:39:22 -0700

    fix indentation and spacing

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -5,21 +5,22 @@ module Text_Parsers
   def anagram_checker(str)
     if vowel_checker(str) === false
       puts "You need to input actual words!"
+    else
+      if uneven_input(str) === true
+        puts 'You need to enter the same number of characters if you want to know if something is an anagram!'
+      else
+        if is_anagram(str) === 'antigram'
+          puts 'No letters match; this is an antigram!'
+        elsif is_anagram(str) === false
+          puts 'Only some letters match. Try again!'
         else
-          if uneven_input(str) === true
-            puts 'You need to enter the same number of characters if you want to know if something is an anagram!'
-          else
-          if is_anagram(str) === 'antigram'
-            puts 'No letters match; this is an antigram!'
-          elsif is_anagram(str) === false
-            puts 'Only some letters match. Try again!'
-          else
-            puts "Congratulations, these are anagrams of each other!"
-          end
+          puts "Congratulations, these are anagrams of each other!"
         end
       end
     end
   end
+end
+
 
   def uneven_input(str)
     if @text.length != @str.length
@@ -72,7 +73,6 @@ module Text_Parsers
       end
     end
   end
-
 
 class AnagramInputs
   include Text_Parsers
